852d78b0b107d6976def388d066cd01a3f9d82a4
Kevin Mudrick <kmudrick@gmail.com>
2022-04-21 17:49:44 -0700

    Build fixes

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,18 @@
-import { parseFile } from "./parser";
-import { JSONSchema } from "./json-schema";
-import { isRight } from "fp-ts/lib/Either";
-import { toDeclarations, getRuntime, getStatic } from "./type-codegen";
-import fs from "fs/promises";
-import path from "path";
+#!/usr/bin/env node
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import { runProgram } from "./program";
 
-export async function runProgram(
-  inputFile: string,
-  outputFile: string
-): Promise<void> {
-  const unparsedSchemas = await parseFile(inputFile);
+const parser = yargs(hideBin(process.argv)).options({
+  input: { type: "string", demandOption: true },
+  output: { type: "string", demandOption: true },
+});
 
-  const parsed: Record<string, JSONSchema> = unparsedSchemas.reduce(
-    (acc, entry) => {
-      const { name, content } = entry;
-      const decoded = JSONSchema.decode(content);
-      if (isRight(decoded)) {
-        const schema = decoded.right;
-        return { ...acc, [name]: schema };
-      } else {
-        console.error(
-          `Could not decode ${name}: ${JSON.stringify(decoded.left)}`
-        );
-        return acc;
-      }
-    },
-    {}
-  );
-  const declarations = toDeclarations(parsed);
-  const runtimeContents = getRuntime(declarations);
-  const staticContents = getStatic(declarations);
-
-  const contents = [staticContents, runtimeContents].join("\n");
-
-  await fs.mkdir(path.dirname(outputFile), { recursive: true });
-  await fs.writeFile(outputFile, contents, "utf8");
-}
+(async () => {
+  const { input, output } = await parser.argv;
+  await runProgram(input, output);
+  return output;
+})().then(
+  (fileWritten) => console.log(`Successfully wrote ${fileWritten}`),
+  (err) => console.error("Unsuccessful", err)
+);
---
+++ src/program.ts
@@ -0,0 +1,39 @@
+import { parseFile } from "./parser";
+import { JSONSchema } from "./json-schema";
+import { isRight } from "fp-ts/lib/Either";
+import { toDeclarations, getRuntime, getStatic } from "./type-codegen";
+import fs from "fs/promises";
+import path from "path";
+
+export async function runProgram(
+  inputFile: string,
+  outputFile: string
+): Promise<void> {
+  const unparsedSchemas = await parseFile(inputFile);
+
+  const parsed: Record<string, JSONSchema> = unparsedSchemas.reduce(
+    (acc, entry) => {
+      const { name, content } = entry;
+      const decoded = JSONSchema.decode(content);
+      if (isRight(decoded)) {
+        const schema = decoded.right;
+        return { ...acc, [name]: schema };
+      } else {
+        console.error(
+          `Could not decode ${name}: ${JSON.stringify(decoded.left)}`
+        );
+        return acc;
+      }
+    },
+    {}
+  );
+  const declarations = toDeclarations(parsed);
+  const runtimeContents = getRuntime(declarations);
+  const staticContents = getStatic(declarations);
+  const ioTsImport = 'import * as t from "io-ts";';
+
+  const contents = [ioTsImport, staticContents, runtimeContents].join("\n");
+
+  await fs.mkdir(path.dirname(outputFile), { recursive: true });
+  await fs.writeFile(outputFile, contents, "utf8");
+}
